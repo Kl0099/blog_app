ada30c1fabc0f9a696a5806d1ee02455fb8b7580
Kl0099 <lodhakrunal464@gmail.com>
2024-02-09 10:58:25 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-const app = require("./app")
-const { connectdatabase } = require("./config/database")
-const cloudinary = require("cloudinary")
-const dotenv = require("dotenv")
+const app = require("./app");
+const { connectdatabase } = require("./config/database");
+const cloudinary = require("cloudinary");
+const dotenv = require("dotenv");
 
-connectdatabase()
-dotenv.config()
+connectdatabase();
+dotenv.config({ path: "/backend/config/config.env" });
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
-})
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`)
-})
+  console.log(`listening on port ${process.env.PORT}`);
+});
