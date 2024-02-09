73d8b58127d2c28067295175c721d68d42ad46c1
Krunal <144949883+Kl0099@users.noreply.github.com>
2024-02-09 11:21:15 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
   cors({
     credentials: true,
-    origin: "https://65c5bb4df54cdd5f381cfaff--dainty-phoenix-2cad54.netlify.app/",
+    origin: "https://65c5bb4df54cdd5f381cfaff--dainty-phoenix-2cad54.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
